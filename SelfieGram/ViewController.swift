4ad1a7c789b3ec53a199a097592ff923e761ccb6
Ishara Jayaweera <isharajayaweera295@gmail.com>
2018-12-05 19:49:12 +0530

    Initial commit

---
+++ SelfieGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfieGram
+//
+//  Created by Ishara Jayaweera on 11/6/18.
+//  Copyright © 2018 Ishara Jayaweera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
